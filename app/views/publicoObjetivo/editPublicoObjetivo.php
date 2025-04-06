ce96256ffc3fbd23c1e1abf5ff019e84d7f360f4
miguelhg14 <amoamimamita12@gmail.com>
2025-04-05 23:40:39 -0500

    trabajo

--- app/views/publicoObjetivo/editPublicoObjetivo.php
+++ app/views/publicoObjetivo/editPublicoObjetivo.php
@@ -1,4 +1,4 @@
-<!-- Asegúrate de incluir FontAwesome en el header de tu layout -->
+<!-- Asegúrate de incluir FontAwesome en el header de tu layout. -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <style>
